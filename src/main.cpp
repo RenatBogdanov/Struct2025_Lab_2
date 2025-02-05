121c5909d84f08999aa5a8579f91dd8802892140
Renat Bogdanov <zeloniyosminog@gmail.com>
2025-02-05 20:13:30 +0300

    Remove include, Start work with dot

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,14 @@
 #include <cstdint>
 #include <string>
 
-#include <graphviz/cgraph.h>
-#include <graphviz/gvc.h>
 #include "game.h"
 
 int main() {
-    game::RenderGame renderGame; // Создание объекта RenderGame
-    game::Game game; // Создание объекта Game
-    game::Node node; // Создание объекта Node
+    game::Game game;
     game::ReadFile read(game);
-    std::cout << "End game" << std::endl;
+    system("dot -Tpng -O graph.dot");
 
-    
-
+    std::cout << "Shutdown" << std::endl;
 
     return 0;
 }
